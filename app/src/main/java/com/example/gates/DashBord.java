1ed8514b7123c07746534fb474df6bdfb7df1ff4
kuldeep23 <kuldeepnitian@gmail.com>
2022-12-28 18:28:45 +0530

    Dashboard

---
+++ app/src/main/java/com/example/gates/DashBord.java
@@ -0,0 +1,14 @@
+package com.example.gates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashBord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_bord);
+    }
+}
