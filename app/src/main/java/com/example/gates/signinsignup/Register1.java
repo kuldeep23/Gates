7f679ea25e27b367371d72b2064aad6074bd2892
optimas23 <kuldeepnitian@gmail.com>
2023-01-17 23:14:46 +0530

    Register Layout

--- app/src/main/java/com/example/gates/signinsignup/Register1.java
+++ app/src/main/java/com/example/gates/signinsignup/Register1.java
@@ -6,12 +6,11 @@ import android.os.Bundle;
 
 import com.example.gates.R;
 
-public class Register extends AppCompatActivity {
+public class Register1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register1);
-
     }
 }
